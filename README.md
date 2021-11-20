86a155ccc57eb0a3b61d140386e0337caa57ad7e
Levent Kantaroğlu <leventkantaroglu@dusyeri.com.tr>
2021-11-20 22:11:51 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A new draft Flutter project.
+
+
---
+++ android/app/src/main/kotlin/com/example/draft_flutter_application/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.draft_flutter_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
